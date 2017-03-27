ef26d8aee2839439b7cdd7b64363e13762799974
Aleks Ulyanov <alulyan@mera.ru>
2017-03-27 19:04:50 +0300

    TC_ACL_2 and TC_ACL_3 are added

--- tests/acl/test_acl_ft_basic.py
+++ tests/acl/test_acl_ft_basic.py
@@ -31,24 +31,51 @@ IP ADDR is 192.168.130.2/24
                          |         |
                          +---------+
 
-TC_ACL_1 basic config
-
-
+Tests:
+TC_ACL_1    Verify that ping from host1 to host2 is absent when ACL is configured as deny for it
+    Steps:
+        1. Actions: Create 3 hosts and a switch, connect hosts to switch; configure IP addresses on ports;
+            Result: there are no any errors;
+        2. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: there is not more than 1 package lost in every iteration;
+        3. Actions: Create access-list TC_ACL_1, set a rule which denies any traffic from Host1 to Host2;
+            Result: there are no any errors;
+        4. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: there is not more than 1 package lost when Host1 pings Host3 and Host2 pings Host3
+                    all packages are lost when Host1 pings Host2
+TC_ACL_2    Verify that ping from host1 to host2 is presented when ACL is configured as permit for it
+    Steps:
+        1. Actions: Create 3 hosts and a switch, connect hosts to switch; configure IP addresses on ports;
+            Result: there are no any errors;
+        2. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: there is not more than 1 package lost in every iteration;
+        3. Actions: Create access-list TC_ACL_2, set a rule which permits any traffic from Host1 to Host2;
+            Result: there are no any errors;
+        4. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: there is not more than 1 package lost in every iteration
+TC_ACL_3    Try to create 1000 access lists. Expected: succesfully
+    Steps:
+        1. Actions: Create 3 hosts and a switch, connect hosts to switch; configure IP addresses on ports;
+            Result: there are no any errors;
+        2. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: there is not more than 1 package lost in every iteration;
+        3. Actions: Create access-list TC_ACL_3, create 1000 rules which deny any traffic from any IP addresses
+            to any interfaces;
+            Result: there are no any errors, 1000 rules are created without any errors;
+        4. Actions: Host1 pings Host2, Host1 pings Host3, Host2 pings Host3;
+            Result: all packages are lost in every iteration
 
 '''
 
-
+import re
 import pytest
 from opstestfw import *
 from opstestfw.switch.CLI import *
+from opstestfw.switch.CLI.showRun import *
 from opstestfw.switch.OVS import *
 import opstestfw.testEnviron
 import opstestfw.Device
-import re
 from opsvsiutils.vtyshutils import *
-
-
-debug=True
 
 #variables:
 slash="/"
@@ -105,7 +132,7 @@ def configure(**kwargs):
     host1 = kwargs.get('host1', None)
     host2 = kwargs.get('host2', None)
     host3 = kwargs.get('host3', None)
-# configure host1
+
     LogOutput('info', "Configuring host1 with IPv4 address")
     retStruct = host1.NetworkConfig(ipAddr = ipHost1,
                                     netMask = maskHost1,
@@ -245,30 +272,99 @@ def enterConfigShell(dut):
     assert retCode == 0, "Failed to enter config terminal"
     return True
 
+#the function expects successfull command execution
 def sendCommand(dut,cmd):
     if (enterConfigShell(dut) is False):
-        LogOutput("error","You are not in configuration mode.\n Failed execute: " + cmd)
-        return False
+        enterConfigShell(dut)
+    retStruct = dut.DeviceInteract(command=cmd)
+    buffer=retStruct.get("buffer")
+    assert retStruct.get("returnCode") == 0,\
+        "There are problems with command execution. Check output below:\n" + buffer
+    # assert not("% Unable" in buffer),\
+    #     "Command is not executed because :\n"+buffer
+    LogOutput("info","Output command: \"" +cmd+"\" is:\n "+buffer)
+    return True
+
+# syntax
+# access-list ip <acl-name>
+def createAccessList(**kwargs):
+    device=kwargs.get("device")
+    accessListName=kwargs.get("name")
+    cmd="access-list ip "+accessListName
+    resultBoolean=sendCommand(device, cmd)
+    runningConfig = device.DeviceInteract(command="do sh run").get("buffer")
+    if not (cmd in runningConfig):
+        resultBoolean = False
+    return resultBoolean
+
+# syntax
+# [<sequence-number>]
+#          {permit|deny}
+#          {any|ah|gre|esp|icmp|igmp|pim|<ip-protocol-num>}
+#          {any|<src-ip-address>[/{<prefix-length>|<subnet-mask>}]}
+#          {any|<dst-ip-address>[/{<prefix-length>|<subnet-mask>}]}
+#          [count] [log]
+#     [no] [<sequence-number>]
+#          {permit|deny}
+#          {sctp|tcp|udp}
+#          {any|<src-ip-address>[/{<prefix-length>|<subnet-mask>}]}
+#          [{eq|gt|lt|neq} <port>|range <min-port> <max-port>]
+#          {any|<dst-ip-address>[/{<prefix-length>|<subnet-mask>}]}
+#          [{eq|gt|lt|neq} <port>|range <min-port> <max-port>]
+#          [count] [log]
+def createRuleAccessList(**kwargs):
+# TO DO: add checking you are in access list configuration mode (switch(config-acl)# )
+    device=kwargs.get("device")                    # deviceObj
+    sequence=str(kwargs.get("sequence",""))        # (<1-4294967295>,None)
+    type=kwargs.get("type","permit")               # (deny | permit)
+    protocol=kwargs.get("protocol","any")          # (any | ah | gre | esp | icmp | igmp |  pim | sctp | tcp | udp | <0-255>)
+    source=kwargs.get("source","any")              # (any | A.B.C.D | A.B.C.D/M | A.B.C.D/W.X.Y.Z)
+    destination=kwargs.get("destination","any")    # (any | A.B.C.D | A.B.C.D/M | A.B.C.D/W.X.Y.Z)
+    statistic=kwargs.get("statistic","")           # {log | count}
+
+    # with destination/source ports
+    if (protocol==("sctp"or"tcp"or"udp")):
+        sourcePort=kwargs.get("sourcePort","")              # {eq|gt|lt|neq} <port>|range <min-port> <max-port>
+        destinationPort=kwargs.get("destinationPort","")    #{eq|gt|lt|neq} <port>|range <min-port> <max-port>
+        cmd=sequence+space+type+space+protocol+space+\
+            source+space+sourcePort+space+\
+            destination+space+destinationPort+space+\
+            statistic
     else:
-        buffer = dut.DeviceInteract(command=cmd).get("buffer")
-        LogOutput("info","Output command: \"" +cmd+"\" is:\n "+buffer)
-        return True
+        cmd=sequence+space+type+space+protocol+\
+            space+source+space+destination+space+statistic
+    resultBoolean=sendCommand(device, cmd)
+#TO DO: Module opstestfw.switch.CLI.showRun shoud be used to check running config,
+# but before need to extend it for ConfigVtyShell mode
+    runningConfig = device.DeviceInteract(command="do sh run").get("buffer")
+    if not (cmd in runningConfig):
+        resultBoolean = False
+    return resultBoolean
 
+#it is not implemented
+def deleteAccessList(**kwargs):
+#TO DO write the method which deletes access lists
+    pass
 
+#it is not implemented
+def deleteRulesAccessList(**kwargs):
+#TO DO write the method which deletes access list rules
+    pass
 
 class Test_ACL_configuration:
-    def setup_class(self):
+    def setup(self):
         # Test object will parse command line and formulate the env
         self.testObj = testEnviron(topoDict=topoDict,defSwitchContext="vtyShell")
         # Get ping topology object
         self.pingTopoObj = self.testObj.topoObjGet()
 
-    def teardown_class(self):
+    def teardown(self):
         self.pingTopoObj.terminate_nodes()
 
-    def test_access_list_permit_in(self):
-        # Initial configuration
-        print Hosts
+    # TC_ACL_1    Verify that ping from host1 to host2 is absent when ACL is configured as deny for it
+    def test_acl_1(self):
+        accessListName="TC_ACL_1"
+
         stepFrame('1', "Configure ip-addresses")
         dut01Obj = self.pingTopoObj.deviceObjGet(device="dut01")
         wrkston01Obj = self.pingTopoObj.deviceObjGet(device="wrkston01")
@@ -285,10 +381,10 @@ class Test_ACL_configuration:
 
         stepFrame('3', "ACL configuration")
         #apply access-list settings
-        assert enterConfigShell(dut01Obj), "Failed to go in Config Shell"
-        sendCommand (dut01Obj,"access-list ip TC_ACL_1")
-        sendCommand (dut01Obj,"deny any "+ipHost1+slash+maskHost1+space+
-                     ipHost2+slash+maskHost2)
+        assert createAccessList(device=dut01Obj, name=accessListName), "Failed to create " + accessListName
+        assert (createRuleAccessList(device=dut01Obj, type="deny",
+                                     source=ipHost1+slash+maskHost1, destination=ipHost2+slash+maskHost2)), \
+            "It is impossible to create rule for access-list"
 
         stepFrame('4', "Check ACL configuration")
         #check access-list blocks ping h1=>h2, but does not block ping h1=>h3 and h2=h3
@@ -296,5 +392,69 @@ class Test_ACL_configuration:
         ping2 = verifyPing(wrkston01Obj, destIPAddress=ipHost3)
         ping3 = verifyPing(wrkston02Obj, destIPAddress=ipHost3)
         assert (ping1 is False) and ((ping2 and ping3) is True),\
-            "Test configuration is not prepared"
+            "Test case is failed"
 
+    # TC_ACL_2    Verify that ping from host1 to host2 is presented when ACL is configured as permit for it
+    def test_acl_2(self):
+        accessListName = "TC_ACL_2"
+
+        stepFrame('1', "Configure ip-addresses")
+        dut01Obj = self.pingTopoObj.deviceObjGet(device="dut01")
+        wrkston01Obj = self.pingTopoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.pingTopoObj.deviceObjGet(device="wrkston02")
+        wrkston03Obj = self.pingTopoObj.deviceObjGet(device="wrkston03")
+        configure(switch1=dut01Obj, host1=wrkston01Obj, host2=wrkston02Obj, host3=wrkston03Obj)
+
+        stepFrame('2', "Check configuration before TC")
+        # Check initial configuration (ping h1=>h2, h1=>h3, h2=>h3)
+        ping1 = verifyPing(wrkston01Obj,destIPAddress=ipHost2)
+        ping2 = verifyPing(wrkston01Obj,destIPAddress=ipHost3)
+        ping3 = verifyPing(wrkston02Obj,destIPAddress=ipHost3)
+        assert (ping1 and ping2 and ping3), "Test configuration is not prepared"
+
+        stepFrame('3', "ACL configuration")
+        assert createAccessList(device=dut01Obj, name=accessListName), "Failed to create " + accessListName
+        assert (createRuleAccessList(device=dut01Obj, source=ipHost1+slash+maskHost1, destination=ipHost2+slash+maskHost2)), \
+            "It is impossible to create rule for access-list"
+
+        stepFrame('4', "Check ACL configuration")
+        #check access-list blocks ping h1=>h2, but does not block ping h1=>h3 and h2=h3
+        ping1 = verifyPing(wrkston01Obj, destIPAddress=ipHost2)
+        ping2 = verifyPing(wrkston01Obj, destIPAddress=ipHost3)
+        ping3 = verifyPing(wrkston02Obj, destIPAddress=ipHost3)
+        assert (ping1 and ping2 and ping3), "Test case is failed"
+
+    #TC_ACL_3   Try to create 1000 access lists. Expected: succesfully
+    def test_acl_3(self):
+        maxQuantityAcls=1000
+        accessListName="TC_ACL_3"
+
+        stepFrame('1', "Configure ip-addresses")
+        dut01Obj = self.pingTopoObj.deviceObjGet(device="dut01")
+        wrkston01Obj = self.pingTopoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.pingTopoObj.deviceObjGet(device="wrkston02")
+        wrkston03Obj = self.pingTopoObj.deviceObjGet(device="wrkston03")
+        configure(switch1=dut01Obj, host1=wrkston01Obj, host2=wrkston02Obj, host3=wrkston03Obj)
+
+        stepFrame('2', "Check configuration before TC")
+        # Check initial configuration (ping h1=>h2, h1=>h3, h2=>h3)
+        ping1 = verifyPing(wrkston01Obj,destIPAddress=ipHost2)
+        ping2 = verifyPing(wrkston01Obj,destIPAddress=ipHost3)
+        ping3 = verifyPing(wrkston02Obj,destIPAddress=ipHost3)
+        assert (ping1 and ping2 and ping3), "Test configuration is not prepared"
+
+        stepFrame('3', "ACL configuration")
+        #apply access-list settings
+        assert createAccessList(device=dut01Obj,name=accessListName), "Failed to create "+accessListName
+        for i in range(1, maxQuantityAcls):
+            assert (createRuleAccessList(device=dut01Obj,type="deny",sequence=i)),\
+                "It is impossible to create more than "+str(i-1)+" access-lists"
+
+        stepFrame('4', "Check ACL configuration")
+        #check access-list blocks ping h1=>h2, but does not block ping h1=>h3 and h2=h3
+        ping1 = verifyPing(wrkston01Obj, destIPAddress=ipHost2)
+        ping2 = verifyPing(wrkston01Obj, destIPAddress=ipHost3)
+        ping3 = verifyPing(wrkston02Obj, destIPAddress=ipHost3)
+        assert not (ping1 and ping2 and ping3), "Test case is failed. Ping shouldn`t work. " \
+                                                "Because there are set deny access lists for any IP addresses" \
+                                                "to any interfaces"
